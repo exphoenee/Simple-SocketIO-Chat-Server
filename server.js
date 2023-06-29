949b1550053ed3e6d1dd6d1ca4ff0c024edb2dcc
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-06-29 12:11:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
@@ -15,6 +16,8 @@ const allowedOrigins = [
   "http://localhost:8081",
 ];
 
+let users = [];
+
 const io = new Server(server, {
   cors: {
     origin: allowedOrigins,
@@ -24,6 +27,22 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log(`A user connected: ${socket.id}`);
+
+  socket.on("login", (data) => {
+    users = [...users, data];
+    socket.broadcast.emit("active_users", users);
+  });
+
+  socket.on("active_users", (users) => {
+    socket.emit("active_users", users);
+  });
+
+  socket.on("logout", (data) => {
+    console.log(`User: ${data.username} logged out`);
+    users = users.filter((user) => user.id !== data.id);
+    console.log(users);
+  });
+
   socket.on("send_message", (data) => {
     console.log(data);
     socket.broadcast.emit("receive_message", data);
